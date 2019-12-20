42712885d065b248aa4e1bda17ee702f44f0a98d
andyzen619 <andyzen619@gmail.com>
2019-12-20 15:36:22 +0000

    Move assets to public/images

--- next-run/src/components/App.js
+++ next-run/src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import './App.css';
 
 // Import semantic UI CSS
@@ -10,7 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={"images/logo.svg"} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
 
           {/* Added semantic UI button to test */}

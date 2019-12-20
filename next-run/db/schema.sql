87c23901604f545f026f5d8635024f5067ab910d
andyzen619 <andyzen619@gmail.com>
2019-12-20 18:23:39 +0000

    Populate schema

--- next-run/db/schema.sql
+++ next-run/db/schema.sql
@@ -2,25 +2,27 @@
 
 Drop table if EXISTS courts cascade;
 
-create table courts ( 
-  id serial primary key NOT null, 
+create table courts (
+  id serial primary key NOT null,
   lat decimal NOT null, 
   lng decimal NOT null, 
   name varchar(255) NOT null, 
   address varchar(255) NOT null, 
-  rating integer, 
-)
+  rating integer
+);
 
+Drop table if EXISTS visits cascade;
 create table visits (
   id serial primary key NOT null,
-  court_id integer references courts(id) on delete cascade
+  court_id integer references courts(id) on delete cascade,
   times_stamp TIME not null
-)
+);
 
+Drop table if EXISTS courtVisits cascade;
 create table courtVisits (
   primary key (court_id, visit_id),
   court_id integer references courts(id) not null,
   visit_id integer references visits(id) not null,
-  quantitiy integer default 1
-)
+  quantity integer default 1
+);
 

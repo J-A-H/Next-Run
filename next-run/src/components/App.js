cc28cf27475ce9a68095fc0e49b285122ac6b860
andyzen619 <andyzen619@gmail.com>
2019-12-21 20:44:59 +0000

    Add google maps react library, add geolocation

--- next-run/src/components/App.js
+++ next-run/src/components/App.js
@@ -1,42 +1,58 @@
-import React, {useState, useEffect } from "react";
+// React components and hooks
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-import useDatabase from '../helpers/useDatabase'
+// Database helper object
+import useDatabase from "../helpers/useDatabase";
+
+//Api calls
+import { Map, GoogleApiWrapper } from "google-maps-react";
 
 const App = props => {
+  const [state, setState] = useState("");
+  const { getAllCourts } = useDatabase(); //Object destructure to use getAllcourts function
 
-  const [state, setState] = useState('');
-  const {getAllCourts} = useDatabase(); //Object destructure to use getAllcourts function
-  
+  defaultProps = () => {
+
+    //Get current location
+    navigator.geolocation.getCurrentPosition(res => {
+      const latitude = res.coords.latitude;
+      const longitude = res.coords.longitude;
+    }, err => {
+      console.log(`Error:${err}`);
+    });
+
+    return {
+      center: {lat: latitude, lng: longitude}, 
+      zoom: 12
+    }
+  }
+
   /**
    * Runs everytime App component is rendered.
    */
   useEffect(() => {
-
     //Makes get request to route in server to query for all courts from our database.
-    getAllCourts().then((res, err)=> {
-      if(err){
-        console.log(err)
+    getAllCourts().then((res, err) => {
+      if (err) {
+        console.log(err);
       }
-      
+
       setState(res.data);
     });
-
   }, []); //Empty arr tells it to only run once after App rendered
 
   return (
     <div className="App">
       <div className="App-header">
         <img src={"images/Next-Run_logo.png"} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-        {/* Added semantic UI button to test */}
-        <button className="ui button">Click Here</button>
       </div>
-      <p className="App-intro">
-        Court Name: {state.name}
-      </p>
+
+      <Map
+
+      />
     </div>
   );
-  }
+};
 
 export default App;

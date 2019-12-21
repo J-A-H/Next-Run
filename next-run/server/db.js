7a8e0fc38456b8f351e8a8b579c053f0e547369e
andyzen619 <andyzen619@gmail.com>
2019-12-21 07:23:08 +0000

    Add comments to server/db.js

--- next-run/server/db.js
+++ next-run/server/db.js
@@ -1,3 +1,8 @@
+/**
+ * This file connects our server to psql database. Your credentials may vary, so update.
+ * You must create database in psql manually first.
+ */
+
 const { Pool } = require('pg');
 
 const pool = new Pool({

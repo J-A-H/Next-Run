abdaace750749e620f4f9d2c54852d66070a59d9
andyzen619 <andyzen619@gmail.com>
2019-12-21 07:24:23 +0000

    Add comments to server/app.js

--- next-run/server/app.js
+++ next-run/server/app.js
@@ -1,10 +1,14 @@
+/**
+ * This is where our server lives. It connects to our database.
+ */
+
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes');
+const indexRouter = require('./routes');//Gets routes.
 
 const app = express();
 
@@ -14,6 +18,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', indexRouter); //Sets routes
 
 module.exports = app;

b45c87c6d95142ec4b2ada1d64afb9fdddaa49b4
andyzen619 <andyzen619@gmail.com>
2019-12-22 19:03:11 +0000

    Add current location marker to map

--- next-run/src/components/App.js
+++ next-run/src/components/App.js
@@ -6,48 +6,51 @@ import "./App.css";
 import useDatabase from "../helpers/useDatabase";
 
 //API calls
-import { GoogleMap, withGoogleMap, withScriptjs } from "react-google-maps";
+import {
+  GoogleMap,
+  withGoogleMap,
+  withScriptjs,
+  Marker
+} from "react-google-maps";
 
 //API keys
 const API_KEY = process.env.REACT_APP_GMAPS_API_KEY;
 const MAP_URL = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry`;
 
-const App = (props) => {
+const App = props => {
   const [state, setState] = useState({
-      courts: [],
-      currentLocation: {}
-    }
-  );
+    courts: [],
+    currentLocation: {}
+  });
   const { getAllCourts } = useDatabase(); //Object destructure to use getAllcourts function
 
   /**
-   * Sets current location and adds to state 
+   * Sets current location and adds to state
    */
-  const setCurrentLocation =  (position) =>{
-
+  const setCurrentLocation = position => {
     setState(prevState => ({
       ...prevState,
       currentLocation: position
     }));
-  }
+  };
 
   /**
    * Generates Map component other props are used with withScriptjs and withGoogleMap
    */
-  const MapComponent = withScriptjs(withGoogleMap((props)=> {
-    return  <GoogleMap
-      defaultZoom={15}
-      defaultCenter={state.currentLocation}
-    >
-    </GoogleMap>
-  }));
-  
+  const MapComponent = withScriptjs(
+    withGoogleMap(props => {
+      return (
+        <GoogleMap defaultZoom={15} defaultCenter={state.currentLocation}>
+          <Marker position={state.currentLocation} />
+        </GoogleMap>
+      );
+    })
+  );
 
   /**
    * Runs everytime App component is rendered.
    */
   useEffect(() => {
-
     //Get all courts from database and updates state
     getAllCourts().then((res, err) => {
       if (err) {
@@ -61,14 +64,17 @@ const App = (props) => {
     });
 
     //Gets current location and saves to state
-    navigator.geolocation.getCurrentPosition(res => {
-
-      setCurrentLocation({lat: res.coords.latitude, lng: res.coords.longitude})
-    }, err => {
-
-      console.log(`Error:${err}`);
-    });
-
+    navigator.geolocation.getCurrentPosition(
+      res => {
+        setCurrentLocation({
+          lat: res.coords.latitude,
+          lng: res.coords.longitude
+        });
+      },
+      err => {
+        console.log(`Error:${err}`);
+      }
+    );
   }, []); //Empty arr tells it to only run once after App rendered
 
   return (
@@ -81,8 +87,7 @@ const App = (props) => {
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `400px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
-      >
-      </MapComponent>
+      ></MapComponent>
     </div>
   );
 };

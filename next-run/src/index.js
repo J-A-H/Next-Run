2e501ca4f9c387ba0c7213588d30e5dffbe0728e
andyzen619 <andyzen619@gmail.com>
2019-12-24 05:29:21 +0000

    removed unused link

--- next-run/src/index.js
+++ next-run/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import CourtDetailsContainer from './components/CourtDetailsContainer';
 

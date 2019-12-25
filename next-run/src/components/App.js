81d9a5b9c08610963e330e1b8d06ec729c572a8e
andyzen619 <andyzen619@gmail.com>
2019-12-25 08:41:33 +0000

    Remove extra closing bracket in app.js

--- next-run/src/components/App.js
+++ next-run/src/components/App.js
@@ -59,7 +59,7 @@ const App = props => {
           setCurrentLocation(coords);
         }
       }
-    });
+    );
   }
 
   //*-------------------------------Custom components----------------------------------------------

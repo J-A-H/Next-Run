0ba147f4b6232032c572e81066594d5fea4e792f
james-truong <james.truong@hotmail.com>
2020-01-09 23:42:56 +0000

    added modals, need to decorate modals

--- next-run/src/components/CourtList.js
+++ next-run/src/components/CourtList.js
@@ -1,17 +1,30 @@
 import React, { Fragment } from "react";
+import { Modal, Button } from "semantic-ui-react";
 import CourtCard from "./CourtCard";
-import {Link} from "react-router-dom";
+import CourtDetailShow from "./CourtDetailShow";
 const CourtList = props => {
-  return (
-    
-    <div>
-      {props.courts.map(court => (
-          <CourtCard key={court.id} court={court} />
-      )
-      )
-    }
+  return props.courts.map(court => (
+    <div className="lit" style={{ zIndex: 100000 }}>
+      <Modal
+        key={court.id}
+        trigger={
+          <Button className="ui button">
+            <CourtCard key={court.id} court={court} />{" "}
+          </Button>
+        }
+        // header={court.name}
+        // content={court.address}
+        // actions={['Snooze', { key: 'done', content: 'Done', positive: true }]}
+      >
+        <CourtDetailShow
+          court={court}
+          getDailyPeakTimes={props.getDailyPeakTimes}
+          getWeeklyPeakTimes={props.getWeeklyPeakTimes}
+          getAllVisits={props.getAllVisits}
+        ></CourtDetailShow>
+      </Modal>
     </div>
-  );
+  ));
 };
 
 export default CourtList;

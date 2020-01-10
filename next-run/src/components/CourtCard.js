11f9a93c5c3d5ef28109f521a74163c7d18d7322
James Truong <james.truong@hotmail.com>
2020-01-10 15:37:42 -0500

    added current activity level to cards

--- next-run/src/components/CourtCard.js
+++ next-run/src/components/CourtCard.js
@@ -3,7 +3,14 @@ import { Card } from "semantic-ui-react";
 import Link from "react-router-dom";
 import RecipeReviewCard from "./CourtDetailShow";
 const CourtCard = props => {
+  const [weekly, setWeekly] = useState(0);
 
+  useEffect(() =>{
+    async function retrieveWeekly(id) {
+      const weeklydata = await props.getWeeklyPeakTimes(id);
+      setWeekly(weeklydata)
+    }
+  }, [weekly])
   return (
     <div
       style={{ opacity: 1, margin: 10, zIndex: 10 }}
@@ -14,7 +21,7 @@ const CourtCard = props => {
           <div className="meta">
             <span className="date">{props.court.address}</span>
           </div>
-          <div className="description">Activity Level: HOT</div>
+          <div className="description">Current Activity Level: {props.playerCount}</div>
         </div>
         <div className="extra content"></div>
       </div>

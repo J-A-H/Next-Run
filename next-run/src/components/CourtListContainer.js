64a0ac94591815090674bd2bdf9d34e815cebede
James Truong <james.truong@hotmail.com>
2020-01-11 13:09:57 -0500

    adding current acitvity level

--- next-run/src/components/CourtListContainer.js
+++ next-run/src/components/CourtListContainer.js
@@ -8,39 +8,64 @@ import CourtCard from "./CourtCard";
 
 const CourtListContainer = props => {
 
+  const [filteredCourts, setfilteredCourts] = useState(null);
+
+  // let low = props.playersCount.filter(court => props.playersCount[court] < 5);
+  // let medium = props.playersCount.filter(
+  //   court => props.playersCount[court] > 5
+  // );
+  // let high = props.playersCount.filter(court => props.playersCount[court] > 10);
+
+  // function showHigh() {
+  //   setfilteredCourts(high);
+  // }
+
+  // function showMedium() {
+  //   setfilteredCourts(medium);
+  // }
+
+  // function showLow() {
+  //   setfilteredCourts(low);
+  // }
+
   return (
     <Fragment>
-      <div 
+      <div
         style={{
           position: "relative",
           zIndex: 3,
           maxWidth: 350,
           overflow: "auto",
-          height: '70vh',
+          height: "70vh",
           backgroundColor: "rgba(52, 52, 52, 0.5)"
         }}
       >
-        
-        <div className="blue filter bar"
+        <div
+          className="blue filter bar"
           style={{
             position: "relative",
             backgroundColor: "rgba(0, 153, 255, 0.5)",
             zIndex: "4",
             height: 60,
             maxWidth: 350,
-            padding: '15px'
+            padding: "15px"
           }}
         >
           <Container />
           <div style={{ float: "left" }}>
-            <DropdownButton id="dropdown-basic-button" title="Filter">
-              <Dropdown.Item onClick="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item onClick="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item onClick="#/action-3">Something else</Dropdown.Item>
+            <DropdownButton
+              id="dropdown-basic-button"
+              title="Filter By Activity Level"
+            >
+              {/* <Dropdown.Item onClick={showHigh}>High</Dropdown.Item>
+              <Dropdown.Item onClick={showMedium}>Medium</Dropdown.Item>
+              <Dropdown.Item onClick={showLow}>
+                Low
+              </Dropdown.Item> */}
             </DropdownButton>
           </div>
         </div>
-        <div className="Court-list" >
+        <div className="Court-list">
           <CourtList
             playersCount={props.playersCount}
             courts={props.courts}

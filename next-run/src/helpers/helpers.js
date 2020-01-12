cfd39c08e0cf5e2495a3e8186f7aaddabba597fe
Andy Liang <andyzen619@gmail.com>
2020-01-11 21:50:40 -0500

    Add helper time stamp function

--- next-run/src/helpers/helpers.js
+++ next-run/src/helpers/helpers.js
@@ -12,7 +12,44 @@ const helpers = () => {
     return '_' + Math.random().toString(36).substr(2, 9);
   }
 
-  return {toKebabCase, randomId}
+  const getTimeStamp = (time) => {
+
+    let result = [];
+
+    //Get hours;
+
+    const date = new Date (time);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    const currentDate = new Date(Date.now());
+
+    const diff = currentDate.getHours - hours;
+
+    if(diff > 23){
+      result.push(`${date.toDateString()} `);
+    }
+    
+    if(hours > 12){
+      result.push(hours - 11);
+    }
+    else{
+      result.push(hours);
+    }
+
+    result.push(`${minutes} `);
+
+    if(hours >= 12){
+      result.push("PM")
+    }
+    else{
+      result.push("AM")
+    }
+
+    return result.join();
+  }
+
+  return {toKebabCase, randomId, getTimeStamp}
 }
 
 export default helpers;

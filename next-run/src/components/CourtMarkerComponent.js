f49e9dabf9aae61cf1c8d16ee6899e1967106300
HermSidhu <h-sidhu@hotmail.com>
2020-01-14 13:21:27 -0500

    map icons are the next-run logo

--- next-run/src/components/CourtMarkerComponent.js
+++ next-run/src/components/CourtMarkerComponent.js
@@ -3,7 +3,7 @@ import {
   Marker 
 } from "react-google-maps";
 
-import myMarker from "../../public/images/Next-Run_logo.png"
+import myMarker from "../../public/images/Next-Run_logo_marker.png"
 /**
    * Generates a court marker  for each court
    * @param {*} param0
